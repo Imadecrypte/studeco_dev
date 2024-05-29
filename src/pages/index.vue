b77ed9089c1399a2fd4c6b5da60d0342e3789ac1
Imadecrypte <130630779+Imadecrypte@users.noreply.github.com>
2024-05-29 17:44:37 +0200

    Ajout sections page d'accueil

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -34,7 +34,7 @@
     <p class="w-14 h-[19px] absolute left-[105px] top-[73px] text-sm text-left text-white">
       LA LISTE
     </p>
-    <p class="w-[91px] h-[19px] absolute left-[194px] top-[73px] text-sm text-left text-white">
+    <p class="w-[92px] h-[19px] absolute left-[194px] top-[73px] text-sm text-left text-white">
       LES E-OFFRES
     </p>
     <p class="w-[49px] h-[19px] absolute left-[316px] top-[73px] text-sm text-left text-white">
@@ -66,27 +66,112 @@
       class="w-[95px] h-[22px] absolute left-[31.5px] top-[583.5px] rounded-[25px] bg-white"
     ></div>
     <div class="w-[67px] h-5">
-      <p class="w-[67px] h-5 absolute left-9 top-[585px] text-[13px] text-left text-black">
+      <p class="w-[69px] h-5 absolute left-9 top-[585px] text-[13px] text-left text-black">
         Voir la carte
       </p>
     </div>
     <p class="absolute left-[85px] top-[451px] text-base text-left uppercase text-[#abd3ff]">
       La Carte de Localisation
     </p>
-    <p class="w-[218px] h-[72px] absolute left-8 top-[494px] text-[10px] text-left text-white">
+    <p class="w-[221px] h-[72px] absolute left-8 top-[494px] text-[10px] text-left text-white">
       Notre site internet intègre une fonctionnalité de carte de géolocalisation interactive, vous
       permettant de découvrir facilement les emplacements physiques des points d'intérêt, des
       magasins ou des événements près de chez vous.
     </p>
     <img
       src="/Back Arrow.svg"
-      class="w-[17px] h-[17px] absolute left-[122.5px] top-[603.5px] object-contain"
+      class="w-[17px] h-[17px] absolute left-[107px] top-[587px] object-contain"
     />
   </div>
   <div class="w-[187px] h-[187px]">
     <img
       src="/map.webp"
-      class="w-[187px] h-[187px] top-[500px] left-[200px] absolute object-cover"
+      class="w-[187px] h-[187px] top-[500px] left-[215px] absolute object-cover"
+    />
+  </div>
+
+  <div class="w-[405px] h-[220px]">
+    <div
+      class="w-[345px] h-[172px] absolute left-[14.5px] top-[710.5px] rounded-[25px] bg-[#ce7891]"
+    ></div>
+    <div
+      class="w-[95px] h-[22px] absolute left-[30.5px] top-[854.5px] rounded-[25px] bg-white"
+    ></div>
+    <div class="w-[67px] h-5">
+      <p class="w-[67px] h-5 absolute left-9 top-[856px] text-[13px] text-left text-black">
+        Voir la liste
+      </p>
+    </div>
+    <img
+      src="/note.webp"
+      class="w-[190px] h-[169px] absolute left-[194.5px] top-[761.5px] object-cover"
+    /><img
+      src="/Back Arrow(1).svg"
+      class="w-[17px] h-[17px] absolute left-[105px] top-[858px] object-contain"
+    />
+    <p class="absolute left-[114px] top-[723px] text-base text-left uppercase text-[#ffbfd2]">
+      La Liste des Offres
+    </p>
+    <p class="w-[185px] h-[66px] absolute left-9 top-[768px] text-[10px] text-left text-white">
+      Une liste de toutes les offres à votre disposition avec différentes possibilités de filtres
+      afin de vous faire gagner du temps mais aussi pour faciliter votre navigation.
+    </p>
+  </div>
+
+  <div class="w-[377px] h-[221px]">
+    <div
+      class="w-[345px] h-[172px] absolute left-[14.5px] top-[996.5px] rounded-[25px] bg-[#32a761]"
+    ></div>
+    <div
+      class="w-[95px] h-[22px] absolute left-[30.5px] top-[1136.5px] rounded-[25px] bg-white"
+    ></div>
+    <div class="w-[76px] h-5">
+      <p class="w-[76px] h-5 absolute left-[34px] top-[1139px] text-xs text-left text-black">
+        Voir les offres
+      </p>
+    </div>
+    <p class="absolute left-24 top-[1010px] text-base text-left uppercase text-[#82ffb4]">
+      les offres numériques
+    </p>
+    <img
+      src="/Back Arrow(2).svg"
+      class="w-[17px] h-[17px] absolute left-[107px] top-[1140px] object-contain"
+    /><img
+      src="/ecran.webp"
+      class="w-[155px] h-[155px] absolute left-[236.5px] top-[1062.5px] object-cover"
+    />
+    <p class="w-[216px] h-[65px] absolute left-9 top-[1050px] text-[10px] text-left text-white">
+      <span class="w-[216px] h-[65px] text-[10px] text-left text-white"
+        >Voici un catalogue des différentes offres disponibles UNIQUEMENT sur Internet. </span
+      ><br /><span class="w-[216px] h-[65px] text-[10px] text-left text-white"
+        >Ce qui vous donnera accès à leurs lien respectif et une courte description.</span
+      >
+    </p>
+  </div>
+
+  <div class="w-[381px] h-56">
+    <div
+      class="w-[345px] h-[172px] absolute left-[14.5px] top-[1296.5px] rounded-[25px] bg-[#d3aa1a]"
+    ></div>
+    <div
+      class="w-[95px] h-[22px] absolute left-[30.5px] top-[1436.5px] rounded-[25px] bg-white"
+    ></div>
+    <div class="w-[58px] h-[17px]">
+      <p class="absolute left-[37px] top-[1439px] text-xs text-left text-black">Voir le TOP</p>
+    </div>
+    <p class="absolute left-[163px] top-[1311px] text-base text-left uppercase text-[#fff3bd]">
+      le top
+    </p>
+    <p class="w-[214px] absolute left-[34px] top-[1364px] text-[10px] text-left text-white">
+      Découvrez un classement des meilleures offres de la communauté ainsi que leurs descriptions et
+      d’une notation.
+    </p>
+    <img
+      src="/trophee.webp"
+      class="w-[169px] h-[169px] absolute left-[226.5px] top-[1351.5px] object-cover"
+    /><img
+      src="/Back Arrow(3).svg"
+      class="w-[17px] h-[17px] absolute left-[103px] top-[1439px] object-contain"
     />
   </div>
 </template>
