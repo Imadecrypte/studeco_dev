cdbd186a2de05a69e723e4fa8a8c91b535efb6d4
Imadecrypte <130630779+Imadecrypte@users.noreply.github.com>
2024-06-01 16:18:42 +0200

    Erreur corrigé header/footer/app

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { onErrorCaptured } from 'vue'
-import { RouterLink, RouterView } from 'vue-router/auto'
+import { RouterView } from 'vue-router/auto'
 import HeaderPage from './components/HeaderPage.vue'
 import FooterPage from './components/FooterPage.vue'
 
@@ -11,20 +11,9 @@ onErrorCaptured((err, instance, info) => {
 </script>
 
 <template>
-    <header>
-        <nav>
-        <ul>
-            <li>
-            <RouterLink to="/" class="text-red-500 underline"> Accueil </RouterLink>
-            </li>
-        </ul>
-        </nav>
-    </header>
-    <HeaderPage />
-    <RouterView v-slot="{ Component }">
-        <Suspense>
-        <component :is="Component" />
-        </Suspense>
-    </RouterView>
-    <FooterPage />
+  <HeaderPage />
+  <main>
+    <RouterView />
+  </main>
+  <FooterPage />
 </template>
--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import { RouterLink } from 'vue-router'
+
 </script>
 
 <template>
