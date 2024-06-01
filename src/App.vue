acb067225716af3cc164bbcf092c0b51cba32952
Imadecrypte <130630779+Imadecrypte@users.noreply.github.com>
2024-06-01 16:07:28 +0200

    Ajout de HeaderPage et de FooterPage

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { onErrorCaptured } from 'vue'
 import { RouterLink, RouterView } from 'vue-router/auto'
+import HeaderPage from './components/HeaderPage.vue'
+import FooterPage from './components/FooterPage.vue'
 
 onErrorCaptured((err, instance, info) => {
   console.error('erreur : ', err, '\ninfo : ', info, '\ncomposant : ', instance)
---
+++ src/components/HeaderPage.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+    <header>
+        <nav>
+        <ul>
+            <li>
+            <RouterLink to="/" class="text-red-500 underline"> Accueil </RouterLink>
+            </li>
+        </ul>
+        </nav>
+    </header>
+    <HeaderPage />
+    <RouterView v-slot="{ Component }">
+        <Suspense>
+        <component :is="Component" />
+        </Suspense>
+    </RouterView>
+    <FooterPage />
+</template>
