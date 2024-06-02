15afe704326017f169a27d1e87ab4583e217ea6f
Imadecrypte <130630779+Imadecrypte@users.noreply.github.com>
2024-06-02 19:33:19 +0200

    1ere correction menu

---
+++ src/components/HeaderPage2.vue
@@ -0,0 +1,33 @@
+<script lang="ts"></script>
+
+<template>
+      <div class="w-[375px] h-[69px]">
+    <div
+      class="w-[375px] h-[69px] absolute left-[-0.5px] top-[-0.5px]"
+      style="
+        background: linear-gradient(
+          134.68deg,
+          #fee47c -0.56%,
+          #25c27c 33.13%,
+          #26b877 55.25%,
+          #47d2e4 79.1%,
+          #86d2fd 87.45%
+        );
+      "
+    ></div>
+    <router-link to="/menu">
+          <div class="w-[43px] h-[22px] absolute left-[320px] top-[20px] cursor-pointer">
+            <div class="w-[33px] h-[3.5px] rounded-[25px] bg-white my-1"></div>
+            <div class="w-[21px] h-1 rounded-[25px] bg-white my-1"></div>
+            <div class="w-[43px] h-1 rounded-[25px] bg-white my-1"></div>
+          </div>
+    </router-link>
+    <img
+      src="/Studeco_2 5.webp"
+      class="w-[47px] h-[51px] absolute left-[9.5px] top-[6.5px] object-cover"
+    /><img
+      src="/User.webp"
+      class="w-[30px] h-[30px] absolute left-[269.5px] top-[19.5px] object-contain"
+    />
+  </div>
+</template>
