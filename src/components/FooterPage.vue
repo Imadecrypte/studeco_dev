e7b584c0fc0cc10104b4d93452adcc885ad152a1
Imadecrypte <130630779+Imadecrypte@users.noreply.github.com>
2024-06-02 10:13:22 +0200

    Ajout du footer

--- src/components/FooterPage.vue
+++ src/components/FooterPage.vue
@@ -1,7 +1,94 @@
 <script setup lang="ts"></script>
 
 <template>
-  <footer>
-    
-  </footer>
+  <div class="w-full flex justify-center mt-6">
+    <footer>
+      <div class="w-[375px] h-[225px] relative">
+        <div
+          class="w-[375px] h-[225px] absolute left-0 top-0"
+          style="
+            background: linear-gradient(
+              134.68deg,
+              #fee47c -0.56%,
+              #25c27c 33.13%,
+              #26b877 55.25%,
+              #47d2e4 79.1%,
+              #86d2fd 87.45%
+            );
+          "
+        ></div>
+        <svg
+          width="375"
+          height="1"
+          viewBox="0 0 375 1"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          class="absolute left-0 top-[225px]"
+          preserveAspectRatio="xMidYMid meet"
+        >
+          <line y1="0.5" x2="375" y2="0.5" stroke="white"></line>
+        </svg>
+        <p
+          class="w-[78px] h-[15px] absolute left-[149px] top-[24px] text-base text-left text-white"
+        >
+          CONTACT
+        </p>
+        <svg
+          width="375"
+          height="1"
+          viewBox="0 0 375 1"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          class="absolute left-0 top-[61px]"
+          preserveAspectRatio="xMidYMid meet"
+        >
+          <line y1="0.5" x2="375" y2="0.5" stroke="white"></line>
+        </svg>
+        <p
+          class="w-[159px] h-[15px] absolute left-[108px] top-[144px] text-base text-left uppercase text-white"
+        >
+          mentions légales
+        </p>
+        <svg
+          width="375"
+          height="1"
+          viewBox="0 0 375 1"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          class="absolute left-0 top-[122px]"
+          preserveAspectRatio="xMidYMid meet"
+        >
+          <line y1="0.5" x2="375" y2="0.5" stroke="white"></line>
+        </svg>
+        <p
+          class="w-[83px] h-[15px] absolute left-[146px] top-[85px] text-base text-left uppercase text-white"
+        >
+          à propos
+        </p>
+        <svg
+          width="375"
+          height="1"
+          viewBox="0 0 375 1"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          class="absolute left-0 top-[179px]"
+          preserveAspectRatio="xMidYMid meet"
+        >
+          <line y1="0.5" x2="375" y2="0.5" stroke="white"></line>
+        </svg>
+        <img
+          src="/logo_insta.svg"
+          class="w-[23px] h-[23px] absolute left-[143.5px] top-[195px] object-cover"
+        />
+        <img
+          src="/logo_x.svg"
+          class="w-[21px] h-[19px] absolute left-[176.5px] top-[198px] object-cover"
+        />
+        <img
+          src="/logo_message.svg"
+          class="w-6 h-[19px] absolute left-[207.5px] top-[198px] object-cover"
+        />
+      </div>
+    </footer>
+  </div>
 </template>
