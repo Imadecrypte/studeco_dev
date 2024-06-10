aab51f6ec0b2c1d1785dd6ede7c9a0d7ded09b5f
Imadecrypte <130630779+Imadecrypte@users.noreply.github.com>
2024-06-10 10:20:07 +0200

    modif menu

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,111 +1,68 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, defineEmits } from 'vue'
+import { RouterLink } from 'vue-router/auto'
+
+const emit = defineEmits(['closeMenu'])
+
+const sections = [
+  { title: 'Accueil', path: '/accueil' },
+  { title: 'Architecte', path: '/architecte' },
+  { title: 'Galerie des oeuvres', path: '/galerie' },
+  { title: 'Frise', path: '/frise' },
+  { title: 'Inscription', path: '/inscription' },
+  { title: 'Connexion', path: '/login' }
+]
+
+const handleLinkClick = () => {
+  emit('closeMenu')
+}
+</script>
 
 <template>
-  <div class="flex justify-center items-center">
-    <div class="w-[375px] h-[700px] relative overflow-hidden bg-white">
-      <svg
-        width="261"
-        height="1"
-        viewBox="0 0 261 1"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        class="absolute left-14 top-[170px]"
-        preserveAspectRatio="xMidYMid meet"
-      >
-        <line y1="0.5" x2="261" y2="0.5" stroke="#848484"></line>
-      </svg>
-      <p class="w-[68px] h-[19px] absolute left-[153px] top-[123px] text-xl text-left text-black">
-        Accueil
-      </p>
-      <svg
-        width="261"
-        height="1"
-        viewBox="0 0 261 1"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        class="absolute left-14 top-[474px]"
-        preserveAspectRatio="xMidYMid meet"
-      >
-        <line y1="0.5" x2="261" y2="0.5" stroke="#848484"></line>
-      </svg>
-      <p class="w-10 h-[19px] absolute left-[167px] top-[427px] text-xl text-left text-black">
-        TOP
-      </p>
-      <svg
-        width="261"
-        height="1"
-        viewBox="0 0 261 1"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        class="absolute left-14 top-[398px]"
-        preserveAspectRatio="xMidYMid meet"
-      >
-        <line y1="0.5" x2="261" y2="0.5" stroke="#848484"></line>
-      </svg>
-      <p class="w-[77px] h-[19px] absolute left-[149px] top-[351px] text-xl text-left text-black">
-        E-Offres
-      </p>
-      <svg
-        width="261"
-        height="1"
-        viewBox="0 0 261 1"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        class="absolute left-14 top-[322px]"
-        preserveAspectRatio="xMidYMid meet"
-      >
-        <line y1="0.5" x2="261" y2="0.5" stroke="#848484"></line>
-      </svg>
-      <p class="w-[94px] h-[19px] absolute left-[140px] top-[275px] text-xl text-left text-black">
-        Les Offres
-      </p>
-      <svg
-        width="261"
-        height="1"
-        viewBox="0 0 261 1"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        class="absolute left-14 top-[246px]"
-        preserveAspectRatio="xMidYMid meet"
-      >
-        <line y1="0.5" x2="261" y2="0.5" stroke="#848484"></line>
-      </svg>
-      <p class="w-20 h-[19px] absolute left-[147px] top-[199px] text-xl text-left text-black">
-        La Carte
-      </p>
-      <div class="w-[134px] h-[47px]">
-        <div
-          class="w-[134px] h-[47px] absolute left-[41.5px] top-[539.5px] rounded-[15px] bg-[#848484]"
-        ></div>
-        <p class="w-[78px] h-[26px] absolute left-[70px] top-[551px] text-xl text-left text-white">
-          Contact
-        </p>
-      </div>
-      <div class="w-[134px] h-[47px]">
-        <div
-          class="w-[134px] h-[47px] absolute left-[197.5px] top-[539.5px] rounded-[15px] bg-[#848484]"
-        ></div>
-        <p class="w-[89px] h-[26px] absolute left-[220px] top-[550px] text-xl text-left text-white">
-          <span class="w-[89px] h-[26px] text-xl text-left uppercase text-white">à </span
-          ><span class="w-[89px] h-[26px] text-xl text-left text-white">propos</span>
-        </p>
-      </div>
-      <div class="w-[134px] h-[47px]">
-        <div
-          class="w-[134px] h-[47px] absolute left-[41.5px] top-[600.5px] rounded-[15px] bg-[#848484]"
-        ></div>
-        <p class="w-[116px] h-[17px] absolute left-[51px] top-[615px] text-sm text-left text-white">
-          Mentions Légales
-        </p>
-      </div>
-      <div class="w-[134px] h-[47px]">
-        <div
-          class="w-[134px] h-[47px] absolute left-[197.5px] top-[600.5px] rounded-[15px] bg-[#848484]"
-        ></div>
-        <p class="w-[98px] h-[26px] absolute left-[215px] top-[612px] text-xl text-left text-white">
-          Inscription
-        </p>
-      </div>
+  <div class="menu-overlay">
+    <div class="menu-content">
+      <ul>
+        <li v-for="section in sections" :key="section.title" class="mb-4">
+          <router-link
+            :to="section.path"
+            class="flex items-center text-left text-xl font-bold focus:outline-none"
+            @click="handleLinkClick"
+          >
+            <img src="/leaf-icon.svg" alt="Leaf icon" class="mr-2" />
+            <span>{{ section.title }}</span>
+          </router-link>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
+
+<style scoped>
+.menu-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 1000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.menu-content {
+  background-color: #f4efe3;
+  padding: 16px;
+  border-radius: 8px;
+}
+
+button {
+  transition: all 0.3s ease;
+}
+
+button:hover {
+  color: #7f9366;
+}
+</style>
+
