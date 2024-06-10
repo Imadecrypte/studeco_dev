7da6f020560bb9ef19c74c0f132c38c968f17ecd
Vbarre2 <vincent.barre@edu.univ-fcomte.fr>
2024-06-10 10:33:45 +0200

    finalisation index

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,18 +1,35 @@
-<script setup lang="ts">
-//
-</script>
+<script setup lang="ts"></script>
+
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&display=swap');
+
+
+.title_card {
+  font-family: 'Jost' , sans-serif;
+}
+
+.card {
+  font-family: 'Jost' , sans-serif;
+}
+
+.access {
+  font-family: 'Jost' , sans-serif;
+}
+
+</style>
 
 <template>
   <div class="w-full max-w-[345px] h-[172px] mx-auto mt-[20px] relative">
     <div class="w-full h-full rounded-[25px] bg-[#5e90d2]"></div>
     <div class="w-[95px] h-[22px] absolute left-[16px] top-[144px] rounded-[25px] bg-white"></div>
     <div class="absolute left-5 top-[146px]">
-      <p class="text-[13px] text-left text-black">Voir la carte</p>
+      <p class="text-[13px] text-left text-black access">Voir la carte</p>
     </div>
-    <p class="absolute left-[85px] top-[12px] text-base text-left uppercase text-[#abd3ff]">
+    <p class="absolute left-[85px] top-[12px] text-base text-left uppercase text-[#abd3ff] title_card">
       La Carte de Localisation
     </p>
-    <p class="absolute left-8 top-[48px] text-[10px] text-left text-white w-[190px] h-[72px]">
+    <p class="absolute left-8 top-[48px] text-[10px] text-left text-white w-[190px] h-[72px] card">
       Notre site internet intègre une fonctionnalité de carte de géolocalisation interactive, vous
       permettant de découvrir facilement les emplacements physiques des points d'intérêt, des
       magasins ou des événements près de chez vous.
@@ -31,7 +48,7 @@
     <div class="w-full h-full rounded-[25px] bg-[#ce7891]"></div>
     <div class="w-[95px] h-[22px] absolute left-[16px] top-[144px] rounded-[25px] bg-white"></div>
     <div class="absolute left-5 top-[146px]">
-      <p class="text-[13px] text-left text-black">Voir la liste</p>
+      <p class="text-[13px] text-left text-black access">Voir la liste</p>
     </div>
     <img
       src="/note.webp"
@@ -41,10 +58,10 @@
       src="/Back Arrow(1).svg"
       class="w-[17px] h-[17px] absolute left-[90px] top-[147px] object-contain"
     />
-    <p class="absolute left-[114px] top-[12px] text-base text-left uppercase text-[#ffbfd2]">
+    <p class="absolute left-[114px] top-[12px] text-base text-left uppercase text-[#ffbfd2] title_card">
       La Liste des Offres
     </p>
-    <p class="absolute left-9 top-[56px] text-[10px] text-left text-white w-[185px] h-[66px]">
+    <p class="absolute left-9 top-[56px] text-[10px] text-left text-white w-[185px] h-[66px] card">
       Une liste de toutes les offres à votre disposition avec différentes possibilités de filtres
       afin de vous faire gagner du temps mais aussi pour faciliter votre navigation.
     </p>
@@ -54,9 +71,9 @@
     <div class="w-full h-full rounded-[25px] bg-[#32a761]"></div>
     <div class="w-[95px] h-[22px] absolute left-[16px] top-[144px] rounded-[25px] bg-white"></div>
     <div class="absolute left-[20px] top-[149px]">
-      <p class="text-xs text-left text-black">Voir les offres</p>
+      <p class="text-xs text-left text-black access">Voir les offres</p>
     </div>
-    <p class="absolute left-24 top-[14px] text-base text-left uppercase text-[#82ffb4]">
+    <p class="absolute left-24 top-[14px] text-base text-left uppercase text-[#82ffb4] title_card">
       Les offres numériques
     </p>
     <img
@@ -67,7 +84,7 @@
       src="/ecran.webp"
       class="w-[155px] h-[155px] absolute left-[calc(50%+30px)] top-[30px] object-cover"
     />
-    <p class="absolute left-9 top-[56px] text-[10px] text-left text-white w-[180px] h-[65px]">
+    <p class="absolute left-9 top-[56px] text-[10px] text-left text-white w-[180px] h-[65px] card">
       Voici un catalogue des différentes offres disponibles UNIQUEMENT sur Internet. Ce qui vous
       donnera accès à leurs lien respectif et une courte description.
     </p>
@@ -77,12 +94,12 @@
     <div class="w-full h-full rounded-[25px] bg-[#d3aa1a]"></div>
     <div class="w-[95px] h-[22px] absolute left-[16px] top-[144px] rounded-[25px] bg-white"></div>
     <div class="absolute left-[20px] top-[149px]">
-      <p class="text-xs text-left text-black">Voir le TOP</p>
+      <p class="text-xs text-left text-black access">Voir le TOP</p>
     </div>
-    <p class="absolute left-[163px] top-[12px] text-base text-left uppercase text-[#fff3bd]">
+    <p class="absolute left-[163px] top-[12px] text-base text-left uppercase text-[#fff3bd] title_card">
       Le top
     </p>
-    <p class="absolute left-[34px] top-[56px] text-[10px] text-left text-white w-[170px]">
+    <p class="absolute left-[34px] top-[56px] text-[10px] text-left text-white w-[170px] card">
       Découvrez un classement des meilleures offres de la communauté ainsi que leurs descriptions et
       d’une notation.
     </p>
