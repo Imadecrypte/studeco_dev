91f9999083f3b95f3680ba573f4a90cfc13bc9e1
Imadecrypte <130630779+Imadecrypte@users.noreply.github.com>
2024-06-11 08:59:13 +0200

    Menu propre et fonctionnel

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -1,14 +1,11 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import { RouterLink } from 'vue-router/auto'
 import Menu from './Menu.vue'
 
 const isMenuOpen = ref(false)
-
 const toggleMenu = () => {
   isMenuOpen.value = !isMenuOpen.value
 }
-
 const closeMenu = () => {
   isMenuOpen.value = false
 }
@@ -54,30 +51,13 @@ const scrollToSection = (sectionId) => {
           <img src="/Studeco_2 4.webp" class="w-[47px] h-[51px] object-cover" />
         </router-link>
         <div class="flex items-center space-x-4">
-          <img src="/User.webp" class="w-[30px] h-[30px] object-contain" />
-          <div @click="toggleMenu">
-            <svg
-              width="99"
-              height="95"
-              viewBox="0 0 99 95"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              class="w-[60px] h-[60px] sm:w-[98.64px] sm:h-[94.29px] relative top-1 sm:top-3.5 cursor-pointer"
-              preserveAspectRatio="none"
-            >
-              <path
-                d="M47.3938 20.7178V22.6514H63.1917H79.0089L78.9511 20.7546L78.8933 18.8762L63.1532 18.8209L47.3938 18.7841V20.7178Z"
-                fill="white"
-              ></path>
-              <path
-                d="M19.6511 47.1444V48.986H49.3204H78.9896V47.1444V45.3029H49.3204H19.6511V47.1444Z"
-                fill="white"
-              ></path>
-              <path
-                d="M19.6896 73.5159L19.7474 75.4127L35.5068 75.4679L51.2469 75.5048V73.5711V71.6374H35.449H19.6318L19.6896 73.5159Z"
-                fill="white"
-              ></path>
-            </svg>
+          <router-link to="/login">
+            <img src="/User.webp" class="w-[30px] h-[30px] object-contain" />
+          </router-link>
+          <div @click="toggleMenu" class="cursor-pointer">
+            <div class="w-[33px] h-[3.5px] rounded-[25px] bg-white my-1"></div>
+            <div class="w-[21px] h-1 rounded-[25px] bg-white my-1"></div>
+            <div class="w-[43px] h-1 rounded-[25px] bg-white my-1"></div>
           </div>
         </div>
       </div>
--- src/components/HeaderPage2.vue
+++ src/components/HeaderPage2.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import { RouterLink } from 'vue-router/auto'
+import { useRouter } from 'vue-router'
 import Menu from './Menu.vue'
 
 const isMenuOpen = ref(false)
-
 const toggleMenu = () => {
   isMenuOpen.value = !isMenuOpen.value
 }
@@ -33,36 +32,21 @@ const toggleMenu = () => {
             class="w-[47px] h-[51px] absolute left-[9.5px] top-[6.5px] object-cover"
           />
         </router-link>
-        <img
-          v-if="!isMenuOpen"
-          src="/User.webp"
-          class="w-[30px] h-[30px] absolute left-[269.5px] top-[19.5px] object-contain"
-        />
-        <div
-          v-if="!isMenuOpen"
-          @click="toggleMenu"
-          class="absolute left-[320px] top-[20px] cursor-pointer"
-        >
-          <div class="w-[33px] h-[3.5px] rounded-[25px] bg-white my-1"></div>
-          <div class="w-[21px] h-1 rounded-[25px] bg-white my-1"></div>
-          <div class="w-[43px] h-1 rounded-[25px] bg-white my-1"></div>
-        </div>
-        <div
-          v-if="isMenuOpen"
-          @click="toggleMenu"
-          class="absolute left-[320px] top-[20px] cursor-pointer"
-        >
-          <div class="close-icon">✖</div>
+        <router-link to="/login">
+          <img
+            src="/User.webp"
+            class="w-[30px] h-[30px] absolute left-[269.5px] top-[19.5px] object-contain"
+          />
+        </router-link>
+        <div @click="toggleMenu" class="absolute left-[320px] top-[20px] cursor-pointer">
+          <div>
+            <div class="w-[33px] h-[3.5px] rounded-[25px] bg-white my-1"></div>
+            <div class="w-[21px] h-1 rounded-[25px] bg-white my-1"></div>
+            <div class="w-[43px] h-1 rounded-[25px] bg-white my-1"></div>
+          </div>
         </div>
       </div>
     </div>
     <Menu v-if="isMenuOpen" @closeMenu="toggleMenu" />
   </div>
 </template>
-
-<style scoped>
-.close-icon {
-  font-size: 24px;
-  color: white;
-}
-</style>
