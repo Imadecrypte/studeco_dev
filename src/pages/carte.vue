d8e17dfa0d389e0c512a8a73700c68a4158710cb
Imadecrypte <130630779+Imadecrypte@users.noreply.github.com>
2024-06-12 09:39:01 +0200

    test géoloc

--- src/pages/carte.vue
+++ src/pages/carte.vue
@@ -55,6 +55,20 @@ onMounted(() => {
   // Fonction d'initialisation qui s'exécute lorsque le DOM est chargé
   initMap()
 })
+
+function geoloc(){ // ou tout autre nom de fonction
+    var geoSuccess = function(position) { // Ceci s'exécutera si l'utilisateur accepte la géolocalisation
+        startPos = position;
+        userlat = startPos.coords.latitude;
+        userlon = startPos.coords.longitude;
+        console.log("lat: "+userlat+" - lon: "+userlon);
+    };
+    var geoFail = function(){ // Ceci s'exécutera si l'utilisateur refuse la géolocalisation
+        console.log("refus");
+    };
+    // La ligne ci-dessous cherche la position de l'utilisateur et déclenchera la demande d'accord
+    navigator.geolocation.getCurrentPosition(geoSuccess,geoFail);
+}
 </script>
 
 <style>
