da5092f48555708b8e20b380d726a698a7fb4f01
Imadecrypte <130630779+Imadecrypte@users.noreply.github.com>
2024-06-13 16:36:10 +0200

    test backend

--- src/backend.ts
+++ src/backend.ts
@@ -2,7 +2,7 @@ import PocketBase from 'pocketbase'
 import { type TypedPocketBase } from './pocketbase-types.js'
 import { ClientResponseError } from 'pocketbase'
 
-export const pb = new PocketBase('https://stud-eco.dezzaznail.fr:443') as TypedPocketBase
+export const pb = new PocketBase(import.meta.env.VITE_URL_POCKETBASE) as TypedPocketBase
 
 export async function registerUser(
   username: string,
